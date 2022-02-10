0eb254850e6349830e3022b3c4270f2df13ca267
dumpserver <dumpserver@hotmail.com>
2022-02-10 06:42:28 -0300

    Include request permission and init map in Recife PE LatLng.

--- app/src/main/java/br/edu/ifpe/tads/pdm/davydadriel/pande1000/MapsActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/davydadriel/pande1000/MapsActivity.java
@@ -1,19 +1,30 @@
 package br.edu.ifpe.tads.pdm.davydadriel.pande1000;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    final static int FINE_LOCATION_REQUEST = 1;
+    private boolean fine_location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,24 +34,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        requestPermission();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng recife = new LatLng(-8.05, -34.9);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(recife));
+
+    }
+
+    private void requestPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        this.fine_location = (permissionCheck == PackageManager.PERMISSION_GRANTED);
+        if (this.fine_location) return;
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                FINE_LOCATION_REQUEST);
+
+        if (mMap != null) {
+            mMap.setMyLocationEnabled(this.fine_location);
+        }
     }
 }
