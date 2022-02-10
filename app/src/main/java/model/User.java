a029071aaed59baa9f3156b0a4b4c0c9ed56a71f
dumpserver <dumpserver@hotmail.com>
2022-02-10 07:52:28 -0300

    Create User model.

---
+++ app/src/main/java/model/User.java
@@ -0,0 +1,18 @@
+package model;
+
+//@IgnoreExtraProperties
+public class User {
+    private String name;
+    private String email;
+    public User() {}
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+}
