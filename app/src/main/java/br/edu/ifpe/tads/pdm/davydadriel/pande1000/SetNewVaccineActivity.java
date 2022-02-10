cdbf840edcec9ed1001b5e9ec651e1e32f1317c7
dumpserver <dumpserver@hotmail.com>
2022-02-10 09:34:03 -0300

    Include longitude and latitude in vaccine, and config to set in Firebase.

--- app/src/main/java/br/edu/ifpe/tads/pdm/davydadriel/pande1000/SetNewVaccineActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/davydadriel/pande1000/SetNewVaccineActivity.java
@@ -1,14 +1,19 @@
 package br.edu.ifpe.tads.pdm.davydadriel.pande1000;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -23,6 +28,11 @@ public class SetNewVaccineActivity extends AppCompatActivity {
 
     DatabaseReference drUser;
     DatabaseReference drChat;
+
+    Vaccine vaccine;
+
+    Double longitude = 0.0;
+    private double wayLatitude = 0.0, wayLongitude = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,22 +56,41 @@ public class SetNewVaccineActivity extends AppCompatActivity {
     }
 
     public void addNewVaccine(View view) {
-//        DatabaseReference drUsers = FirebaseDatabase.getInstance().getReference("users");
-//
-//        System.out.println("Verificando objeto");
-//        System.out.println(drUsers.child(mAuth.getCurrentUser().getUid()));
+
+        EditText vaccine_name = findViewById(R.id.vaccine_name);
+
+        FusedLocationProviderClient fusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(this);
+
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(this, location -> {
+            if(location!=null) {
+                this.sendToBase(
+                    view,
+                    location.getLatitude(),
+                    location.getLongitude()
+                );
+            }
+        });
+    }
+
+    private void sendToBase(View view, Double latitude, Double longitude){
 
         FirebaseDatabase fbDB = FirebaseDatabase.getInstance();
         FirebaseUser fbUser = mAuth.getCurrentUser();
         drUser = fbDB.getReference("users/" + fbUser.getUid());
         drChat = fbDB.getReference("vaccines");
-
         EditText vaccine_name = findViewById(R.id.vaccine_name);
 
-        drChat.push().setValue(new Vaccine(fbUser.getUid(), vaccine_name.getText().toString()));
+        drChat.push().setValue(new Vaccine(
+            fbUser.getUid(),
+            vaccine_name.getText().toString(),
+            latitude,
+            longitude));
 
         Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
+
     }
 
 }
--- app/src/main/java/model/Vaccine.java
+++ app/src/main/java/model/Vaccine.java
@@ -6,13 +6,20 @@ import com.google.firebase.database.IgnoreExtraProperties;
 public class Vaccine {
     private final String user_id;
     private final String name;
+    private final Double latitude;
+    private final Double longitude;
+
     public Vaccine() {
         this.user_id = null;
         this.name = null;
+        this.latitude = null;
+        this.longitude = null;
     }
-    public Vaccine(String user_id, String name) {
+    public Vaccine(String user_id, String name, Double longitude, Double latitude) {
         this.user_id = user_id;
         this.name = name;
+        this.longitude = longitude;
+        this.latitude = latitude;
     }
 
     public String getUserId() {
@@ -21,5 +28,13 @@ public class Vaccine {
 
     public String getName() {
         return name;
+    }
+
+    public Double getLatitude(){
+        return latitude;
+    }
+
+    public Double getLongitude(){
+        return longitude;
     }
 }
