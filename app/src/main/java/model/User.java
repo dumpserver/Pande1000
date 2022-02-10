30505640c7bf47232477e8dc4d34bed246f1b8f6
dumpserver <dumpserver@hotmail.com>
2022-02-10 07:53:49 -0300

    Include RealTimeDatabase in project.

--- app/src/main/java/model/User.java
+++ app/src/main/java/model/User.java
@@ -1,6 +1,8 @@
 package model;
 
-//@IgnoreExtraProperties
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
 public class User {
     private String name;
     private String email;
