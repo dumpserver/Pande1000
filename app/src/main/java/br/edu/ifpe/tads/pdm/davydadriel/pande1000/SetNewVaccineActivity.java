4f3aa8208ee2f5a809d7141d28f320d54283dc7f
dumpserver <dumpserver@hotmail.com>
2022-02-11 10:35:14 -0300

    Include static spin in set new vaccine activity.

--- app/src/main/java/br/edu/ifpe/tads/pdm/davydadriel/pande1000/SetNewVaccineActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/davydadriel/pande1000/SetNewVaccineActivity.java
@@ -9,7 +9,9 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -40,6 +42,21 @@ public class SetNewVaccineActivity extends AppCompatActivity {
 
         this.mAuth = FirebaseAuth.getInstance();
         this.authListener = new FirebaseAuthListener(this);
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.vaccine_spinner);
+        //create a list of items for the spinner.
+        String[] items = new String[]{
+            "Pfizer",
+            "CoronaVac",
+            "Johnson",
+            "AstraZeneca",
+        };
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
     }
 
     @Override
@@ -75,14 +92,17 @@ public class SetNewVaccineActivity extends AppCompatActivity {
         FirebaseUser fbUser = mAuth.getCurrentUser();
         drUser = fbDB.getReference("users/" + fbUser.getUid());
         drChat = fbDB.getReference("vaccines");
-        EditText vaccine_name = findViewById(R.id.vaccine_name);
+//        EditText vaccine_name = findViewById(R.id.vaccine_name);
+        Spinner spinner = (Spinner) findViewById(R.id.vaccine_spinner);
+        String vaccine_name = spinner.getSelectedItem().toString();
 
         // Get current date.
         String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
 
         drChat.push().setValue(new Vaccine(
             fbUser.getUid(),
-            vaccine_name.getText().toString(),
+//            vaccine_name.getText().toString(),
+            vaccine_name,
             currentDate,
             latitude,
             longitude));
